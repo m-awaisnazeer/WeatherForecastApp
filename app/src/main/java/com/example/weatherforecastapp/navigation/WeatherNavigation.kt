f3e33a8df735e836dd65cc33ed2fae25cf5051b0
Awais Ahmad <awais.ahmad@venturedive.com>
2022-10-16 12:03:22 +0500

    Hilt and Navigation setup

---
+++ app/src/main/java/com/example/weatherforecastapp/navigation/WeatherNavigation.kt
@@ -0,0 +1,18 @@
+package com.example.weatherforecastapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherforecastapp.screen.WeatherSplashScreen
+
+@Composable
+fun WeatherNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = WeatherScreens.SplashScreen.name){
+        composable(WeatherScreens.SplashScreen.name){
+            WeatherSplashScreen(navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherforecastapp/screen/WeatherSpalshScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weatherforecastapp.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun WeatherSplashScreen(navController: NavHostController) {
+    Text(text = "Splash Screen")
+}
